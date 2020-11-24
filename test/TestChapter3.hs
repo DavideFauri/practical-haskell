fd77c0d0b2882d91849841392dd4994b081d22fb
Davide Fauri <davide.fauri@gmail.com>
2020-11-24 19:07:20 +0100

    scaffolding for Chapter 3 tests
    (cherry picked from commit 2fe34c6a3b31c331a3da4184aef2a1920521829f)

---
+++ test/TestChapter3.hs
@@ -0,0 +1,11 @@
+module TestChapter3 where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+import Test.Tasty.QuickCheck
+
+chapter3Tests :: TestTree
+chapter3Tests =
+  testGroup
+    "Chapter 3"
+    []
