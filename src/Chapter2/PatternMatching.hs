7b539adf68e2949504afa22e802a8443835fd465
Davide Fauri <davide.fauri@gmail.com>
2020-11-24 17:48:34 +0100

    add missing Example in PatternMatching

--- src/Chapter2/PatternMatching.hs
+++ src/Chapter2/PatternMatching.hs
@@ -12,6 +12,11 @@ clientName (GovOrg name) = name
 clientName (Company name _ _ _) = name
 clientName (Individual (Person fNm lNm _) _) = fNm ++ " " ++ lNm
 
+f :: Client -> String
+f client = case client of
+  Company _ _ (Person name _ _) "Boss" -> name ++ " is the boss"
+  _ -> "There is no boss"
+
 fibonacci :: Integer -> Integer
 fibonacci 0 = 0
 fibonacci 1 = 1
