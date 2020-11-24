45931b7d348f2c733295a12224cdbd0be0a4a9fc
Davide Fauri <davide.fauri@gmail.com>
2020-11-24 12:30:30 +0100

    add Exercise 2-4

--- test/TestChapter2.hs
+++ test/TestChapter2.hs
@@ -2,11 +2,13 @@
 
 module TestChapter2 where
 
+import Chapter2.DataTypes
 import Chapter2.ListsOfLists
 import Test.Tasty
 import Test.Tasty.ExpectedFailure
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
+import Unsafe.Coerce
 
 chapter2Tests :: TestTree
 chapter2Tests =
@@ -18,6 +20,8 @@ chapter2Tests =
       testMatchesGuards,
       testRecords
     ]
+
+-- EXERCISE 2-1
 
 testListsOfLists :: TestTree
 testListsOfLists =
@@ -32,20 +36,6 @@ testListsOfLists =
       testOnlyOneElement,
       testConcatenateTwo
     ]
-
-testDataTypes :: TestTree
-testDataTypes = testGroup "Exercise 2-4" []
-
-testPatternMatching :: TestTree
-testPatternMatching = testGroup "Exercise 2-5" []
-
-testMatchesGuards :: TestTree
-testMatchesGuards = testGroup "Exercise 2-6" []
-
-testRecords :: TestTree
-testRecords = testGroup "Exercise 2-7" []
-
--- EXERCISE 2-1
 
 testOneTwoThree :: TestTree
 testOneTwoThree =
@@ -153,8 +143,103 @@ arbitraryTwoOrMoreLists = do
 
 -- EXERCISE 2-4
 
+testDataTypes :: TestTree
+testDataTypes =
+  testGroup
+    "Exercise 2-4"
+    [ testGenderShowable,
+      testClientGeneration,
+      testTimeMachine
+    ]
+
+testGenderShowable :: TestTree
+testGenderShowable =
+  testProperty "Gender datatype is Showable" $
+    forAll
+      (arbitrary :: Gen Gender)
+      propGender
+  where
+    genders = ["Male", "Female", "Unknown"]
+    minProbEachGender = 30.0 -- percent
+    propGender g =
+      checkCoverage $
+        coverTable "Gender" (zip genders $ repeat minProbEachGender) $
+          tabulate "Gender" [show g] $
+            show g `elem` genders
+
+instance Arbitrary Gender where
+  arbitrary = oneof [pure Male, pure Female, pure Unknown]
+
+testClientGeneration :: TestTree
+testClientGeneration =
+  testProperty "Client datatype can be generated" $
+    forAll
+      (arbitrary :: Gen Client)
+      propClient
+  where
+    clients = ["GovOrg", "Company", "Individual"]
+    minProbEachClient = 30.0 -- percent
+    getConstructor = head . words . show
+    propClient c =
+      checkCoverage $
+        coverTable "Client" (zip clients $ repeat minProbEachClient) $
+          tabulate "Client" [getConstructor c] $
+            getConstructor c `elem` clients
+
+instance Arbitrary Client where
+  arbitrary =
+    oneof
+      [ GovOrg <$> arbitrary,
+        Company <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary,
+        Individual <$> arbitrary <*> arbitrary
+      ]
+
+instance Arbitrary Person where
+  arbitrary = Person <$> arbitrary <*> arbitrary <*> arbitrary
+
+testTimeMachine :: TestTree
+testTimeMachine =
+  testProperty "TimeMachine datatype can be generated" $
+    forAll
+      (arbitrary :: Gen TimeMachine)
+      propTimeMachine
+  where
+    getConstructor = head . words . show
+    propTimeMachine tm@(TimeMachine _ _ _ dir _) =
+      -- checkCoverage $
+      -- coverTable "Direction" [("Forward'", 30.0), ("Backward'", 30.0), ("BiDirectional'", 30.0)] $
+      -- tabulate "Direction" [show (unsafeCoerce dir :: AnyTimeDirection)] $
+      getConstructor tm == "TimeMachine"
+
+data AnyTimeDirection = Forward' | Backward' | BiDirectional' deriving (Show)
+
+instance Arbitrary AnyTimeDirection where
+  arbitrary = oneof [pure Forward', pure Backward', pure BiDirectional']
+
+--FIX How to get rid of unsafeCoerce?
+-- TL;DR: I have to use an arbitrary type because I don't know the implementation of TimeDirection
+-- Does TimeDirection have two constructors? Three? More? I cannot derive an Arbitrary instance for an unknown sum type!
+-- And even if I can, how do I check the coverage on that data type? I don't know the constructor names!
+-- So best thing I can do is to coerce arbitrray values of my type to TimeDirection, hoping to match its cardinality.
+-- But when coercing back for tabulating (see commented code above), the internal representation stay the same and that's bad.
+-- ex. coercing an AnyTimeDirection (cardinality 3) to a TimeDirection that's only Forward | Backward (cardinality 2) results in:
+-- ~33% Forward, ~67% Backward <-- all the "BiDirectional'" get coerced into the last sum type!
+-- The internal representation stays the same, though, so when coercing back to AnyTimeDirection:
+-- ~33% Forward', ~33% Backward', ~33% BiDirectional' <-- I get NO insight on the cardinality of TimeDirection
+instance Arbitrary TimeMachine where
+  arbitrary = TimeMachine <$> arbitrary <*> arbitrary <*> arbitrary <*> unsafeCoerce (arbitrary :: Gen AnyTimeDirection) <*> arbitrary
+
 -- EXERCISE 2-5
+
+testPatternMatching :: TestTree
+testPatternMatching = testGroup "Exercise 2-5" []
 
 -- EXERCISE 2-6
 
+testMatchesGuards :: TestTree
+testMatchesGuards = testGroup "Exercise 2-6" []
+
 -- EXERCISE 2-7
+
+testRecords :: TestTree
+testRecords = testGroup "Exercise 2-7" []
